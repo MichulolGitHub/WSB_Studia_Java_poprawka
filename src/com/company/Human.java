bbf0d11db616b9e48da6ee136ce668715accac77
MichulolGitHub <durlak.m@wp.pl>
2020-08-01 21:54:06 +0200

    Zadanie 6: one more thing - DONE to 4 point

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import com.company.devices.Car;
+
 public class Human {
 
     public Animal pet;
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import com.company.devices.Car;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -36,7 +38,13 @@ public class Main {
         System.out.println("---------------------------------");
         Human me = new Human("Tom","XYZ");
         Car car = new Car ("A7","Audi",2019);
+        Car car1 = new Car ("A7","Audi",2019);
+        Car car2 = new Car ("A7","Audi",2019);
         me.car = car;
+        System.out.println(me.car);
+        System.out.println(car1);
+        System.out.println(car2);
+
 
     }
 }
